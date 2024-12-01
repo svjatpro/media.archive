fa73a6466ae175b62467d89024aa4865fe85d0ab
Sviatoslav Prokipets <s.prokipets@steel.mil.ua>
2024-12-01 17:44:36 +0200

    add client dummy project

---
+++ Media.Archive.WinClient/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Media.Archive.WinClient
+{
+    [Register( "AppDelegate" )]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
